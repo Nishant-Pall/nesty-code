d0cea501d09bd45272e7a8f37077188709f2ce00
Nishant-Pall <palln6935@gmail.com>
2023-10-20 13:17:38 +0530

    Add dto with req res decorator features

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,37 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { Response } from 'express';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
   @Get()
-  findAll(@Req() request: Request, @Res() response: Response) {
-    response.send('All cats');
+  findAll(
+    @Req() request: Request,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    response.status(HttpStatus.OK);
+    return 'All cats';
+  }
+  @Get(':id')
+  findCat(
+    @Req() request: Request,
+    @Param() params: any,
+    @Res() response: Response,
+  ) {
+    response.send(`cat ${params.id}`);
+  }
+
+  @Post()
+  create(@Body() createCatDto: CreateCatDto) {
+    return `This action adds a new cat ${JSON.stringify(createCatDto)}`;
   }
 }
